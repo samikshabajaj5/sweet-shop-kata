541abaa0ebb921d749cf6e99c817fb2e0287454b
Samiksha <samikshab005@gmail.com>
2025-11-15 00:26:33 +0530

    feat(app): database connection  setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import db from "./utils/db";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+db.authenticate().then(() => console.log("DB connected"));
 
 // Health check route
 app.get("/", (req, res) => {
