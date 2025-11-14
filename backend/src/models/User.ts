af27eb5e1240713318d8389edf0d0904ed01e645
Samiksha <samikshab005@gmail.com>
2025-11-15 01:27:28 +0530

    fix(types): add full TypeScript typings to User model to expose email, password, role and other attributes

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,32 +1,64 @@
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model, Optional } from "sequelize";
 import db from "../utils/db";
 
-class User extends Model {}
+interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+interface UserCreationAttributes
+  extends Optional<UserAttributes, "id" | "role"> {}
+
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public role!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
 
 User.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
+
     password: {
-      type: DataTypes.STRING(255),
+      type: DataTypes.STRING,
       allowNull: false,
     },
+
     role: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: "user", // NEW → default user role
+      defaultValue: "user",
     },
   },
   {
     sequelize: db,
-    modelName: "User",
+    tableName: "users",
   },
 );
 
