dafec3f55d6d79d7be45c231183b4b2e36b8d7d2
Samiksha <samikshab005@gmail.com>
2025-11-15 02:43:32 +0530

    fix(schema): Fixed the restock schema according to the test

--- backend/src/schemas/inventory.schema.ts
+++ backend/src/schemas/inventory.schema.ts
@@ -3,13 +3,6 @@ import { z } from "zod";
 export const purchaseSchema = z.object({
   id: z.string().min(1, "Sweet ID is required"),
 });
-
 export const restockSchema = z.object({
-  id: z.string().min(1, "Sweet ID is required"),
-  amount: z
-    .number({
-      error: "Amount is required",
-      //   invalid_type_error: "Amount must be a number",
-    })
-    .positive("Amount must be greater than zero"),
+  amount: z.number().min(1, "Amount must be positive"),
 });
