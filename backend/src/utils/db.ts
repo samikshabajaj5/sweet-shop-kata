1876ec2b6b5fefd4b2fa48d83b84e5f44be9bc26
Samiksha <samikshab005@gmail.com>
2025-11-15 00:32:18 +0530

    refactor(db):added environment configuration

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,10 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  process.env.DB_URL || "postgres://postgres:password@localhost:5432/sweetshop",
-  {
-    logging: false,
-  },
-);
+const db = new Sequelize(process.env.DB_URL as string, {
+  logging: false,
+});
 
 export default db;
